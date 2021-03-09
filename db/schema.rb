07cb92c47282a1e3f24bf5b1b4f99d8e01629e31
Kevin Lenell <krlenell@gmail.com>
2021-03-09 13:10:03 -0800

    migration to change types in students table

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_204243) do
+ActiveRecord::Schema.define(version: 2021_03_09_210115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "students", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.text "name", null: false
-    t.boolean "active", default: false
+    t.string "name", null: false
+    t.boolean "active", default: false, null: false
     t.boolean "minor", null: false
-    t.text "phone"
-    t.text "email"
-    t.text "school"
-    t.text "address"
+    t.string "phone"
+    t.string "email"
+    t.string "school"
+    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
