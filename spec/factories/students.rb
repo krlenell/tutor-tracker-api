0be5583d28da8d4f8a9f8d5706f8d86f541d7d9d
Kevin Lenell <krlenell@gmail.com>
2021-03-09 13:15:40 -0800

    changed properties in student factorybot

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :student do
-    student_name { "Stupid Test Boy" }
-    is_active { false }
-    is_minor { false }
-    student_phone { "1234567" }
-    student_email { "xxxallthekills69@hotmail.com" }
+    name { "Stupid Test Boy" }
+    active { false }
+    minor { false }
+    phone { "1234567" }
+    email { "xxxallthekills69@hotmail.com" }
     school { "School of Dumbo" }
     address { "1234 Stinky Lane" }
   end
