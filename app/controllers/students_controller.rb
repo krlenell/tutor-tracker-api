9a33b46a377f645b119f5757ad51c2c2bd846ef4
Kevin Lenell <krlenell@gmail.com>
2021-03-23 15:21:34 -0700

    setup factorybot properly, added tests for get

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,10 +2,6 @@
 class StudentsController < ApplicationController
   def index
     student_list = Student.all
-    if student_list.length.zero?
-      render json: {error: 'no students'}, status: :not_found
-    else
-      render json: student_list, status: :ok
-    end
+    render json: student_list, status: :ok
   end
 end
