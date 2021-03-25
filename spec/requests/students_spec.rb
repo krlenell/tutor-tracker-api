0ca21b932f5ecfb5bfcfd31050984ce93294e660
Kevin Lenell <krlenell@gmail.com>
2021-03-24 19:16:05 -0700

    minor improvements to spec

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Students', type: :request do
   describe 'GET /index' do
+
+    it 'successfully responds with json' do
+      get students_path
+      expect(response.content_type).to include('application/json')
+      expect(response).to have_http_status(:ok)
+    end
+
     context 'when there are no students' do
       it 'responds with 200 status' do
-        get '/students'
+        get students_path
         expect(response).to have_http_status(:ok)
       end
 
-      it 'returns json error message' do
-        get '/students'
+      it 'returns empty array' do
+        get students_path
         expect(JSON.parse(response.body)).to be_empty
       end
     end
@@ -17,15 +24,16 @@ RSpec.describe 'Students', type: :request do
     context 'when there are students' do
       it 'responds with ok status' do
         create(:student)
-        get '/students'
+        get students_path
         expect(response).to have_http_status(:ok)
       end
 
       it 'response body is not empty' do
         create(:student)
-        get '/students'
+        get students_path
         expect(JSON.parse(response.body)).not_to be_empty
       end
     end
   end
+
 end
